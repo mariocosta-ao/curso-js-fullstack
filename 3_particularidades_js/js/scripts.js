d728f30db037e3b7d5d4a32a3d24e6154002ee36
Mário Costa <mariocosta2760@gmail.com>
2025-03-08 01:10:12 +0100

    using console.log

---
+++ 3_particularidades_js/js/scripts.js
@@ -0,0 +1,16 @@
+console.log("Primeiro");
+console.log("Último");
+console.log("Mais um console.log");
+var a = 1; // Declara uma variável
+a = 'teste';
+console.log(a);
+
+// Este é um comentário
+
+/*
+  Este
+  é um
+  comentário de
+  múltiplas
+  linhas
+*/
